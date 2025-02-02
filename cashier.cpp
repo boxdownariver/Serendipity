9f72a75561a2138846f90ea64d03cb3f94073b83
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-02-02 11:35:34 -0800

    Update cashier.cpp

--- cashier.cpp
+++ cashier.cpp
@@ -4,11 +4,21 @@
 
 include <iostream>
 
+struct bookType {
+	string date;
+	int quantity;
+	string isbn;
+	string title;
+	float price;
+	float total;
+};
 using namespace std;
 
 int main (){
+	bookType bookOne;
 
-
+	// Pass to a function to get the data.
+	cout << "hello\n";
 
 
 	return 0;
