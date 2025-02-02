94bf55e9d86275f30243c3dbf3f534a63e5b50dc
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-02-02 12:32:58 -0800

    Update cashier.cpp

--- cashier.cpp
+++ cashier.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 using namespace std;
 
@@ -17,12 +18,17 @@ struct bookType {
 };
 
 void askData (bookType &books);
+void FormatReport (const bookType &books);
 
 int main (){
 	bookType bookOne;
 	// Pass to a function to get the data.
 	askData ( bookOne);
 
+	// Pass to a function to show the formatted report
+	FormatReport (bookOne); 
+
+	
 	return 0;
 }
 
@@ -41,9 +47,42 @@ void askData (bookType &books)
 	cout << "\nTitle : ";
 	getline (cin, books.title);
 
-	cout << "\nPrice  : ";
+	cout << "\nPrice ($) : ";
 	cin >> books.price;
 	
 	// Calculate the total.
 	books.total = books.price * books.quantity;
+}
+
+
+
+void FormatReport (const bookType &books)
+{
+	float subtotal;
+	float tax_amt;
+	const float taxPct = 0.06;
+	float totalAfterTax;
+
+	subtotal = books.total;
+	tax_amt = taxPct * subtotal;
+	totalAfterTax = tax_amt + subtotal;
+	
+	cout << "--------------------------------------------------------------------------------\n";
+	cout << "-                                                                              -\n";
+	cout << "- Serendipity Book Sellers                                                     -\n";
+	cout << "-                                                                              -\n";	
+	cout << "Date: " << books.date << endl;
+	cout << "-" << setw(5) << left << "Qty" << setw(14) << left << "ISBN" << setw(38) << left << "Title";
+	cout << setw (12) << left << "Price" << setw (9) << left << "Total" << "-\n";
+	cout << "--------------------------------------------------------------------------------\n"; 
+
+	cout << "-" << setw(3) << right << books.quantity << setw (16) << right << books.isbn << setw (35) << left. books.title;
+	cout << setw (4) << right << "$" << setw (7) << right << books.price << setw (5) << right << "$" << setw(8) << right << books.total << "-\n";
+	cout << setw (58) << left << "-" << "Subtotal    $" << setw (7) << right << subtotal << setw (2) << right << "-\n";
+	cout << setw (58) << left << "-" << "Tax         $" << setw (7) << right << tax_amt << setw (2) << right << "-\n";
+	cout << setw (58) << left << "-" << "Total       $" << setw (7) << right << total << setw (2) << right << "-\n";
+	cout << "-                                                                              -\n";
+	cout << "- " << setw (77) << left << "Thank you for shopping at Serendipity" << "-\n";
+
+	
 }
