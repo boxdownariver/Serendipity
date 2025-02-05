1d279d008f780d765c18d606520d7d5316c552f3
AmITeejAtSchool <tdignan1@saddleback.edu>
2025-02-05 14:34:47 -0800

    Basic version of the book info screen, no resize functionality

--- bookInfo.cpp
+++ bookInfo.cpp
@@ -1,11 +1,25 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-	cout << "Testing 123/n";
-	cout << "New code";
+	cout << left << "-----------------------------------------------------------\n";
+	cout << 			 "-               Serendipity Booksellers                   -\n";
+	cout << 			 "-                    Book Information                     -\n";
+	cout << 			 "-                                                         -\n";
+	cout << 			 "-    ISBN:                                                -\n";
+	cout << 			 "-    Title:                                               -\n";
+	cout <<			 "-    Author:                                              -\n";
+	cout <<			 "-    Publisher:                                           -\n";
+	cout << 			 "-    Date Added:                                          -\n";
+	cout << 			 "-    Quantity-On-Hand:                                    -\n";
+	cout <<			 "-    Wholesale Cost:                                      -\n";
+	cout <<			 "-    Retail Price:                                        -\n";
+	cout <<			 "-                                                         -\n";
+	cout << 			 "-----------------------------------------------------------\n";
+
 	return 0;
 }
 
