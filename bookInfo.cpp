ec9dcda6db089b60d3caefeb61fc9553c68477fa
AmITeejAtSchool <tdignan1@saddleback.edu>
2025-02-05 14:37:22 -0800

    Edited the spacing so it doesn't look goofy

--- bookInfo.cpp
+++ bookInfo.cpp
@@ -6,19 +6,19 @@ using namespace std;
 int main()
 {
 	cout << left << "-----------------------------------------------------------\n";
-	cout << 			 "-               Serendipity Booksellers                   -\n";
-	cout << 			 "-                    Book Information                     -\n";
-	cout << 			 "-                                                         -\n";
-	cout << 			 "-    ISBN:                                                -\n";
-	cout << 			 "-    Title:                                               -\n";
-	cout <<			 "-    Author:                                              -\n";
-	cout <<			 "-    Publisher:                                           -\n";
-	cout << 			 "-    Date Added:                                          -\n";
-	cout << 			 "-    Quantity-On-Hand:                                    -\n";
-	cout <<			 "-    Wholesale Cost:                                      -\n";
-	cout <<			 "-    Retail Price:                                        -\n";
-	cout <<			 "-                                                         -\n";
-	cout << 			 "-----------------------------------------------------------\n";
+	cout <<         "-               Serendipity Booksellers                   -\n";
+	cout <<         "-                    Book Information                     -\n";
+	cout <<         "-                                                         -\n";
+	cout <<         "-    ISBN:                                                -\n";
+	cout <<         "-    Title:                                               -\n";
+	cout <<         "-    Author:                                              -\n";
+	cout <<         "-    Publisher:                                           -\n";
+	cout <<         "-    Date Added:                                          -\n";
+	cout <<         "-    Quantity-On-Hand:                                    -\n";
+	cout <<         "-    Wholesale Cost:                                      -\n";
+	cout <<         "-    Retail Price:                                        -\n";
+	cout <<         "-                                                         -\n";
+	cout <<         "-----------------------------------------------------------\n";
 
 	return 0;
 }
