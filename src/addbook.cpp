9633d011717735b8b1449be002927131bffd9259
Micah <micahkrosby@gmail.com>
2025-02-19 02:51:11 -0800

    Pain

--- src/addbook.cpp
+++ src/addbook.cpp
@@ -1,8 +1,11 @@
+#include <form.h>
 #include "headers/addbook.h"
 #include "headers/menuDipity.h"
 #ifndef signal
 #include <signal.h>
 #endif
+//#include <string>
+#include <cstring>
 
 void ahandleSignal(const int signal);
 
@@ -14,12 +17,16 @@ void addBook(BookType bookList[20], int &currentBookCount) {
 	WINDOW *notification;
 	ITEM **items;
 	MENU *mainMenu;
+	FIELD *userInputField;
+	FORM *userInputForm;
 	MenuLines mainMenuInfo;
 	BookType bookBuffer;
 	struct sigaction sa;
 	int input;
 	int choice;
+	int formch;
 	char inputChar;
+	char *fieldBuffer;
 	bool continueMenu;
 
 	std::vector<std::string> menuListing = { "Set Title", "Set ISBN",
@@ -29,7 +36,7 @@ void addBook(BookType bookList[20], int &currentBookCount) {
 
 	mainMenuInfo.menuName = "Add a Book";
 	mainMenuInfo.menuLines = menuListing;
-	mainMenuInfo.longestMenuLength = 20;
+	mainMenuInfo.longestMenuLength = 21;
 
 	sa.sa_flags = 0;
 	sa.sa_handler = ahandleSignal;
@@ -54,24 +61,41 @@ void addBook(BookType bookList[20], int &currentBookCount) {
 	post_menu(mainMenu);
 	mvwprintw(mainWindow, getmaxy(mainWindow) - 2,
 			(getmaxx(mainWindow) - 38) / 2,
-			"Select [0-%d] or navigate to module...",
+			"Select [0-%d] or navigate to choice...",
 			menuListing.size() - 1);
 	wrefresh(mainWindow);
 
 	notification = newwin(1, 3 * COLS / 5, 9 * LINES / 10, COLS / 5);
+	keypad(notification, true);
 	bookDisplayWindow = derwin(mainWindow, menuListing.size(),
 			getmaxx(mainWindow) / 2 - 1, 3, getmaxx(mainWindow) / 2);
 	box(bookDisplayWindow, 0, 0);
 	wrefresh(bookDisplayWindow);
 
+	userInputField = new_field(1, getmaxx(notification) - 1, 0, 1, 0, 0);
+	set_field_back(userInputField, A_UNDERLINE);
+	field_opts_off(userInputField, O_AUTOSKIP);
+
+	FIELD *fields[2] { userInputField, nullptr };
+	userInputForm = new_form(fields);
+	set_form_win(userInputForm, notification);
+	set_form_sub(userInputForm,
+			derwin(notification, getmaxy(notification),
+					getmaxx(notification), 0, 0));
+
 	//Main program loop
 	while (currentBookCount < 20 && continueMenu) {
 		if (astateProvider) {
 			delwin(bookDisplayWindow);
-			refreshWindowMiddleSplit(mainMenu, mainWindow,
-					notification, mainMenuInfo);
+			refreshWindowMiddleSplit(mainMenu, mainWindow, notification,
+					mainMenuInfo);
+			mvwprintw(mainWindow, getmaxy(mainWindow) - 2,
+					(getmaxx(mainWindow) - 38) / 2,
+					"Select [0-%d] or navigate to choice...",
+					menuListing.size() - 1);
 			bookDisplayWindow = derwin(mainWindow, menuListing.size(),
-			getmaxx(mainWindow) / 2 - 1, 3, getmaxx(mainWindow) / 2);
+					getmaxx(mainWindow) / 2 - 1, 3,
+					getmaxx(mainWindow) / 2);
 			box(bookDisplayWindow, 0, 0);
 			wrefresh(bookDisplayWindow);
 
@@ -84,47 +108,81 @@ void addBook(BookType bookList[20], int &currentBookCount) {
 		case 10:
 			choice = item_index(current_item(mainMenu));
 			if (choice < menuListing.size() && choice != -1) {
-				switch (choice) {
-				case 0:
-					//Get book title
-					//bookBuffer.bookTitle =;
-					break;
-				case 1:
-					//Get isbn
-					//bookBuffer.isbn =;
-					break;
-				case 2:
-					//Get author
-					//bookBuffer.author =;
-					break;
-				case 3:
-					//Get publisher
-					//bookBuffer.publisher =;
-					break;
-				case 4:
-					//Get date added
-					//bookBuffer.dateAdded =;
-					break;
-				case 5:
-					//Get quantity on hand
-					//bookBuffer.qtyOnHand =;
-					break;
-				case 6:
-					//Get wholesale value
-					//bookBuffer.wholesale =;
-					break;
-				case 7:
-					//Get retail value
-					//bookBuffer.retail =;
-					break;
-				case 8:
+				if (choice < 8) {
+					if (choice > 4) {
+						set_field_type(userInputField, TYPE_NUMERIC);
+					} else {
+						set_field_type(userInputField, NULL);
+					}
+					post_form(userInputForm);
+					wrefresh(notification);
+					while ((formch = wgetch(notification)) != 10) {
+						switch (formch) {
+						case KEY_LEFT:
+							form_driver(userInputForm, REQ_PREV_CHAR);
+							break;
+						case KEY_RIGHT:
+							form_driver(userInputForm, REQ_NEXT_CHAR);
+							break;
+						case KEY_BACKSPACE:
+							form_driver(userInputForm, REQ_PREV_CHAR);
+							form_driver(userInputForm, REQ_DEL_CHAR);
+							break;
+						default:
+							form_driver(userInputForm, formch);
+							break;
+						}
+						//wprintw(notification, "|");
+						wrefresh(notification);
+					}
+					fieldBuffer = new char[getmaxx(notification)];
+					memcpy(fieldBuffer, field_buffer(userInputField, 0),
+							getmaxx(notification));
+					switch (choice) {
+					case 0:
+						//Get book title
+						bookBuffer.bookTitle = fieldBuffer;
+						break;
+					case 1:
+						//Get isbn
+						bookBuffer.isbn = fieldBuffer;
+						break;
+					case 2:
+						//Get author
+						bookBuffer.author = fieldBuffer;
+						break;
+					case 3:
+						//Get publisher
+						bookBuffer.publisher = fieldBuffer;
+						break;
+					case 4:
+						//Get date added
+						bookBuffer.dateAdded = fieldBuffer;
+						break;
+					case 5:
+						//Get quantity on hand
+						//bookBuffer.qtyOnHand =;
+						break;
+					case 6:
+						//Get wholesale value
+						//bookBuffer.wholesale =;
+						break;
+					case 7:
+						//Get retail value
+						//bookBuffer.retail =;
+						break;
+					default:
+
+						break;
+					}
+					unpost_form(userInputForm);
+					wrefresh(notification);
+					delete[] fieldBuffer;
+				}
+				if (choice == 8) {
 					bookList[currentBookCount] = bookBuffer;
 					currentBookCount++;
-					break;
-				default:
-					continueMenu = 0;
-					break;
-				}
+				} else if (choice == 9) continueMenu = 0;
 			}
 			break;
 		case KEY_UP:
@@ -150,6 +208,8 @@ void addBook(BookType bookList[20], int &currentBookCount) {
 			break;
 		}
 	}
+	free_form(userInputForm);
+	free_field(userInputField);
 	wclear(notification);
 	delwin(notification);
 	delwin(bookDisplayWindow);
