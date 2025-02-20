915bb46c21bff2b8b4d9446342b99f5b6e2e6c50
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-02-19 22:21:34 -0800

    small changes

--- src/headers/bookInfo.h
+++ src/headers/bookInfo.h
@@ -1,7 +1,7 @@
-#ifndef BOOKINFO_H
-#define BOOKINFO_H
+#define bookInfo
 
+#ifndef invmenu
 #include "invmenu.h"
+#endif
 
 int mainbookInfo ( const BookType array[], const int &bookIndex );
-#endif
