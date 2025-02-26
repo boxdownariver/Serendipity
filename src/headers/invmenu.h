1acd6f63d0c05be75fec1581ef5e0e857539fe92
Micah <micahkrosby@gmail.com>
2025-02-25 21:15:08 -0800

    Example documentation in invmenu.h

--- src/headers/invmenu.h
+++ src/headers/invmenu.h
@@ -2,14 +2,22 @@
 #include <string>
 #endif
 #define invmenu
+
+/**
+ * BookType\n
+ * Definition for books in the database.\n
+ * Currently, all invmenu interfaces use this type.\n
+ * Not to be confused with CashierBookType
+ */
 struct BookType {
-	std::string bookTitle;
-	std::string isbn;
-	std::string author;
-	std::string publisher;
-	std::string dateAdded;
-	int qtyOnHand;
-	double wholesale;
-	double retail;
+	std::string bookTitle; ///< Book Title
+	std::string isbn;      ///< Book ISBN-13 code
+	std::string author;    ///< Book Author
+	std::string publisher; ///< Book Publisher
+	std::string dateAdded; ///< The date at which the book was added
+	int qtyOnHand;         ///< Current book count for this book
+	double wholesale;      ///< Wholesale price of book
+	double retail;         ///< Retail value of book
 };
+
 int main_invmenu(BookType (&books)[20], int &bookCount);
