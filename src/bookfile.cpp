1aa591a9ef1b32da07d752a28538e03dbe3cdc89
Micah <micahkrosby@gmail.com>
2025-02-25 23:01:09 -0800

    Missed bookfile.cpp

--- src/bookfile.cpp
+++ src/bookfile.cpp
@@ -1,6 +1,20 @@
+/**********************************************************************//**
+ * READ FILE INTO BOOK LIST
+ * ________________________________________________________________________
+ * This program reads a file into the book list. File is "booklist.dat",
+ * input is comma-separated.
+ * @author Micah Krosby
+ * @file bookfile.cpp
+ * ________________________________________________________________________
+ *
+ * PROCESS-
+ * 		BookType[20] books, int bookNumber -> Book array with current size
+ * 			to be mutated by the file input program.
+ *************************************************************************/
 #include "headers/bookfile.h"
 #include <fstream>
 
+///readFile(BookType[20], int) -> Stores file entries as members of books.
 void readFile(BookType books[20], int &bookNumber) {
 	BookType bookBuffer;
 	std::ifstream file;
