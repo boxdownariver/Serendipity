cd4cd26b588f8f58424bb562ade3c9b72a7e9900
cuculeha <86766249+cuculeha@users.noreply.github.com>
2025-03-01 11:51:50 -0800

    aligned with uml given

--- src/bookType.cpp
+++ src/bookType.cpp
@@ -25,6 +25,21 @@ bookType::bookType()
 }
 
 /**
+* Constructor with parameters for bookType class. Initializes member variables with given values.
+*/
+bookType ::bookType (string title, string isbn , string auth, string pub, string date , int qty, double ws, double ret)
+{
+	setTitle (title);
+	setISBN (isbn);
+	setAuthor (auth);
+	setPub (pub);
+	setDateAdded (date);
+	setQtyOnHand (qty);
+	setWholesale (ws);
+	setRetail (ret);
+}
+
+/**
  * printBookInfo function prints the information of the book in a formatted manner.
  * 
  * The function prints details like ISBN, title, author, publisher, date added, 
@@ -39,13 +54,31 @@ void bookType :: printBookInfo () const
 	cout <<         "║  ISBN             : " << setw (78) << left << getISBN() << " ║\n";
 	cout <<         "║  Title            : " << setw(78) << left << getTitle() << " ║\n";
 	cout <<         "║  Author           : " << setw (78) << left << getAuthor() << " ║\n";
-	cout <<         "║  Publisher        : " << setw (78) << left << getPublisher() << " ║\n";
-	cout <<         "║  Date Added       : " << setw (78) << left << getDate() << " ║\n";
-	cout <<         "║  Quantity-On-Hand : " << setw (78) << left << getQuantity() << " ║\n";
+	cout <<         "║  Publisher        : " << setw (78) << left << getPub() << " ║\n";
+	cout <<         "║  Date Added       : " << setw (78) << left << getDateAdded() << " ║\n";
+	cout <<         "║  Quantity-On-Hand : " << setw (78) << left << getQtyOnHand() << " ║\n";
 	cout <<         "║  Wholesale Cost   : " << setw (78) << left << fixed << setprecision(2) << getWholesale() << " ║\n";
 	cout <<         "║  Retail Price     : " << setw(78) << left << fixed << setprecision(2) << getRetail() << " ║\n";
 	cout <<         "║                                                                                                    ║\n";
 	cout <<         "╚════════════════════════════════════════════════════════════════════════════════════════════════════╝\n";
+}
+
+/**
+ * equalData function compares two books to see if they have identical attributes.
+ * 
+ * Parameter anotherBook is the book object to compare the current object with.
+ * @return true if all attributes of both books are identical.
+ * @return false if any attribute of the two books differs.
+ */
+bool bookType::equalData (const bookType anotherBook) const 
+{
+	if (bookTitle == anotherBook.bookTitle && isbn == anotherBook.isbn && author == anotherBook.author &&
+		publisher == anotherBook.publisher && dateAdded == anotherBook.dateAdded && qtyOnHand == anotherBook.qtyOnHand &&
+		wholesale == anotherBook.wholesale && retail == anotherBook.retail)
+		{
+			return true;
+		}
+	return false;
 }
 
 /**
