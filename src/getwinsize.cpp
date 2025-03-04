ce50e6f96f996da09ae5404b7c7af87c849a877c
Micah Krosby <96504420+boxdownariver@users.noreply.github.com>
2025-03-03 17:50:20 -0800

    Documentation and minor cleanup for window size

--- src/getwinsize.cpp
+++ src/getwinsize.cpp
@@ -1,3 +1,10 @@
+/**
+* GET WINDOW SIZE
+*____________________________________________________________
+* This program retrieves the window size for use with setw().
+* @author Micah Krosby
+* @file getwinsize.cpp
+*/
 #include "headers/getwinsize.h"
 #include <sys/ioctl.h>
 #include <fcntl.h>
@@ -5,11 +12,12 @@
 
 namespace getwinsize {
 	int getwidth() {
-		struct winsize ws;
-		int filedesc;
+		struct winsize ws;	/// Winsize object
+		int filedesc;		/// File descriptor: int
 
 		filedesc = open("/dev/tty", O_RDWR);
-		if (filedesc >= 0 && !(ioctl(filedesc, TIOCGWINSZ, &ws))); else ws.ws_col = 0;
+		if (filedesc >= 0 && !(ioctl(filedesc, TIOCGWINSZ, &ws))); 
+		else ws.ws_col = 0;
 		close(filedesc);
       		return ws.ws_col;	
 	}
